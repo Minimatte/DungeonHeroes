537e028bdc62ac3a05006ca011c31c4c315bac59
Fredrik <Fredrik.tumlin@gmail.com>
2016-10-08 18:25:16 +0200

    Modified main menum changed how npc speech bubble look, reworked first boss a little bit, needs more work.

--- Assets/Enemies/Bosses/Beholder/Beholder.cs
+++ Assets/Enemies/Bosses/Beholder/Beholder.cs
@@ -21,12 +21,12 @@ public class Beholder : Boss {
     protected override void Attack() {
         if (!active)
             return;
-        
+
         if (health.hasShield) {
             var rand = Random.Range(0, 3);
             switch (rand) {
                 case 0:
-                    StartCoroutine(SpinAttack());
+                    StartCoroutine(FocusAttack());
                     break;
                 case 1:
                     StartCoroutine(ShotgunAttack());
@@ -36,25 +36,29 @@ public class Beholder : Boss {
                     break;
             }
         } else {
-            StartCoroutine(FocusAttack());
+            StartCoroutine(SpinAttack());
+
         }
     }
 
     private IEnumerator FocusAttack() {
-        yield return new WaitForSeconds(0.05f);
-        currentCooldown = 0.1f;
+        for (int i = 0; i < 3; i++) {
+            currentCooldown = 0.6f;
 
-        var direction = (target.position - transform.position).normalized;
-        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            var direction = (target.position - transform.position).normalized;
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, angle));
-        Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, angle - 5));
-        Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, angle + 5));
+            Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, angle));
+            yield return new WaitForSeconds(0.15f);
+        }
+
     }
 
     private IEnumerator SpinAttack() {
-        for (int i = 0; i < 940; i += 15) {
-            currentCooldown = attackProperties.cooldown;
+        for (int i = 0; i < 365; i += 15) {
+            currentCooldown = 0.4f;
+            //currentCooldown = 1.2166666f;
+            //currentCooldown = attackProperties.cooldown;
             Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, i));
             Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, i + 180));
             yield return new WaitForSeconds(0.05f);
@@ -62,8 +66,9 @@ public class Beholder : Boss {
     }
 
     private IEnumerator ShotgunAttack() {
-        for (int i = 360; i > 0; i -= 15) {
-            currentCooldown = attackProperties.cooldown;
+        for (int i = 90; i > 0; i -= 15) {
+            //currentCooldown = 0.3f;
+            currentCooldown = 0.5f;
             Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, i));
             Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, i + 90));
             Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, i + 180));
@@ -73,17 +78,18 @@ public class Beholder : Boss {
     }
 
     private IEnumerator Wave() {
-        for (int x = 0; x < 5; x++) {
+        for (int x = 0; x < 1; x++) {
             var direction = (target.transform.position - transform.position).normalized;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
             for (int i = -45; i < 45; i += 10) {
-                currentCooldown = attackProperties.cooldown;
+                //currentCooldown = attackProperties.cooldown;
                 direction = Quaternion.AngleAxis(i, Vector3.up) * direction;
                 Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, i + angle));
             }
 
             yield return new WaitForSeconds(1);
         }
+        currentCooldown = 0.2f;
     }
 }
